3aa5957831c1185df40629ea6f44340cf9144dd2
hans <xx>
2020-07-14 11:59:50 +0800

    init

---
+++ install_scripts.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+pip install torch torchvision
+pip install opencv-python
+pip install mmcv cvbase
+
+
+cd ./models/FlowNet2_Models/resample2d_package/
+rm -rf *_cuda.egg-info build dist __pycache__
+python3 setup.py develop
+
+cd ../correlation_package
+rm -rf *_cuda.egg-info build dist __pycache__
+python3 setup.py develop
+
+cd ../channelnorm_package
+rm -rf *_cuda.egg-info build dist __pycache__
+python3 setup.py develop
+cd ../..
